d686a2cd58b82970549cd069d25f39e55a32358a
Pietro <pietro>
2018-05-07 21:02:16 +0100

    Power array: accepted

--- power_array.cpp
+++ power_array.cpp
@@ -1,10 +1,8 @@
 /*
  * Competive Programming - UniPi.
- * Pietro Paolini - 2017
  * Souce: http://codeforces.com/contest/86/problem/D
  * Complexity: O(SQRT(N) * N)
  */
-#include <iostream>
 #include <vector>
 #include <set>
 #include <utility>
@@ -12,51 +10,54 @@
 #include <cstring>
 #include <map>
 #include <cmath>
+#define MAX_LENGTH  (200000 + 5)
+#define MAX_COLORS (1000000 + 5)
 using namespace std;
 
 class query {
 public:
   int l, r, pos;
-  friend ostream& operator<<(ostream& os, const query& one);
+  //  friend ostream& operator<<(ostream& os, const query& one);
 };
 
-ostream& operator<<(ostream& os, const query& one) {
-  os << "{" << one.l << "," << one.r << "}";
-  return os;
-}
+// ostream& operator<<(ostream& os, const query& one) {
+//   os << "{" << one.l << "," << one.r << "}";
+//   return os;
+// }
 
 int bucket_size;
-long long  counter[1000000 + 1] = { 0 };
-int input[200000 + 2] = { 0 };
-query Q[200000 + 2] = { 0 };
+long long  counter[MAX_COLORS] = { 0 };
+int input[MAX_LENGTH] = { 0 };
+query Q[MAX_LENGTH];
+long long results[MAX_LENGTH] = { 0 };
+		 
+
 bool right_order(query one, query two) {
-  if ((one.l / bucket_size) < (two.l / bucket_size))
-    return true;
-  else if ((one.l / bucket_size) > (two.l / bucket_size))
-    return false;
-  else
+  if ((one.l / bucket_size) == (two.l / bucket_size))
     return one.r < two.r;
+  else
+    return (one.l) < (two.l);
 }
 
-
-
 int main() {
-  std::ios_base::sync_with_stdio(false);
-  cin.tie(0);
+  // std::ios_base::sync_with_stdio(false);
+  // cin.tie(0);
   int                    size_array, num_queries;
-  vector<long long>      results;
   
-  cin >> size_array >> num_queries;
+  //  cin >> size_array >> num_queries;
+  scanf("%d %d", &size_array, &num_queries);
+
   bucket_size = round((float) (size_array / sqrt(size_array)));
-  results.reserve(num_queries + 1);
   for (int i = 0; i < size_array; i++) {
-    cin >> input[i];
+    scanf("%d", &input[i]);
+	  //    cin >> input[i];
   }
 
   // Store all queries in the buckets
   for (int i = 0; i < num_queries; i++) { // O(Q * lgQ)
-    cin >>  Q[i].l;
-    cin >>  Q[i].r;
+    // cin >>  Q[i].l;
+    // cin >>  Q[i].r;
+    scanf("%d %d", &Q[i].l, &Q[i].r);
     Q[i].pos = i;
   }
 
@@ -68,7 +69,8 @@ int main() {
   sort (Q, Q + num_queries, right_order);// O(SQRT(N)* LG(SQRT(N)))
   for (int i = 0; i < num_queries; i++) {
       int query_l, query_r;
-      
+
+      //      cout << Q[i] << endl;
       // Query's boundaries
       query_l = Q[i].l - 1;
       query_r = Q[i].r - 1;
@@ -96,7 +98,8 @@ int main() {
       results[Q[i].pos] = result;
     }
   for (auto i = 0; i < num_queries; ++i) { // O(Q) Q = queries
-    cout << results[i] << endl;
+    printf("%I64d\n", results[i]);
+    //cout << results[i] << '\n';
   }
   return 0;
 }
