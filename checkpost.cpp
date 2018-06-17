a03b9d1b826a5fbababddd42d100e86ce9da8a13
Pietro <pietro>
2018-06-17 01:01:48 +0100

    Checkpost: OK

--- checkpost.cpp
+++ checkpost.cpp
@@ -6,108 +6,92 @@
 #include <set>
 #include <cstring>
 #include <algorithm>
+#include <map>
 
 using namespace std;
-
-class F {
+class SCC {
 public:
+  SCC(int n, int c): node(n), cost(c) { };
   int node;
-  int time;
   int cost;
-  F(): node(0), time(0), cost(0) { };
 };
 
-bool order_by_time(F one, F two)
-{
-  return one.time > two.time;
-}
-
-bool order_by_cost(F one, F two)
+bool order_by_cost(SCC one, SCC two)
 {
   return one.cost < two.cost;
 }
 
 class graph {
 public:
-  int **            matrix;
-  int               junctions_number, time, sccs_counter;
-  int *             visited, * visited_t;
-  F *               f;
-  vector<vector<F>> sccs;
-  graph(int junctions, vector<int>& junction_costs);
-  void process_edge(int u, int v);
-  void dfs(int node);
-  void dfs_t(int node);
-  pair<int, int> solution();
+  vector<list<int>>     adjacent;
+  vector<list<int>>     adjacent_transpose;
+  int64_t               junctions_number, time, sccs_counter;
+  vector<bool>          visited;
+  vector<int64_t>       cost;
+  map<int, int, greater<int>>         f;
+  vector<vector<SCC>>   sccs;
+  graph(int64_t junctions, vector<int64_t>& junction_costs);
+  void process_edge(int64_t u, int64_t v);
+  void dfs(int64_t node);
+  void dfs_t(int64_t node);
+  pair<int64_t, int64_t> solution();
 };
 
 
-graph::graph(int junctions, vector<int>& junction_costs): sccs(junctions)
+graph::graph(int64_t junctions, vector<int64_t>& junction_costs): cost(junction_costs),
+								  sccs(junctions),
+								  visited(junctions),
+								  adjacent(junctions),
+								  adjacent_transpose(junctions)
 {
   this->junctions_number = junctions;
-  this->visited = new int[junctions]();
-  this->visited_t = new int[junctions]();
-  this->matrix = new int*[junctions];
-  this->f = new F[junctions]();
-  for (auto i = 0; i < junctions; i++) {
-    this->matrix[i] = new int[junctions]();
-    this->f[i].cost = junction_costs[i];
-  }
-  
   this->time = this->sccs_counter = 0;
 }
 
-void graph::process_edge(int u, int v)
+void graph::process_edge(int64_t u, int64_t v)
 {
-  this->matrix[u][v] = 1;
+  this->adjacent[u].push_back(v);
+  this->adjacent_transpose[v].push_back(u);
 }
 
 
-void graph::dfs(int node) {
+void graph::dfs(int64_t node) {
   this->visited[node] = 1;
   this->time++;
-  for (int i = 0; i < this->junctions_number; i++) {
-    if (!matrix[node][i])
-      continue;
-    
-    if (!this->visited[i]) // Don't visit it if it's been visited already
-	dfs(i);
+  for (auto it = this->adjacent[node].begin(); it != this->adjacent[node].end(); ++it) {
+
+    if (!this->visited[*it]) // Don't visit it if it's been visited already
+	dfs(*it);
   }
 
   // Store exit node's time
   this->time++;
-  this->f[node].node = node;
-  this->f[node].time = time;
+  this->f[time] = node;
+  //  this->f[time] = node;
 }
 
-void graph::dfs_t(int node)
+void graph::dfs_t(int64_t node)
 {
-  if (this->visited_t[node])
+  if (this->visited[node])
     return;
 
-  cout << __FUNCTION__ << node << " Adding at " << sccs_counter << endl;
+  //  cout << __FUNCTION__ << node << " Adding at " << sccs_counter << endl;
 
   
-  sccs[this->sccs_counter].push_back(f[node]);
-  this->visited_t[node] = 1;
+  sccs[this->sccs_counter].push_back(SCC(node, this->cost[node]));
+  this->visited[node] = 1;
 
-  vector<F> children;
-  for (int i = 0; i < this->junctions_number; i++) {
-    if (!matrix[i][node]) // Transpose
+  for (auto it = this->adjacent_transpose[node].begin(); it != this->adjacent_transpose[node].end(); ++it) {
+  
+    if (this->visited[*it])
       continue;
 
-    children.push_back(this->f[i]);
-  }
-
-  sort(children.begin(), children.end(), order_by_time);
-  for (auto it = children.begin(); it != children.end(); ++it) {
-    if (!this->visited_t[it->node])
-      dfs_t(it->node);
+    dfs_t(*it);
   }
 }
    
-pair<int, int> graph::solution() {
-  int cost, ways;
+pair<int64_t, int64_t> graph::solution() {
+  int64_t cost, ways;
 
   cost = 0;
   ways = 1;
@@ -115,10 +99,17 @@ pair<int, int> graph::solution() {
     if (it->empty())
       continue;
 
+    // cout << "SCC ";
+    // for (auto scc_item = it->begin(); scc_item != it->end(); ++scc_item) {
+    //   cout << scc_item->node << ",";
+    // }
+    // cout << endl;
+    
     sort(it->begin(), it->end(), order_by_cost);
 
-    int count_min = 1;
+    int64_t count_min = 1;
     cost += it->begin()->cost;
+    //cout << "cost += " << it->begin()->cost << endl;
 
     // Add all the ones having the same cost as the form a possible solution
     for (auto scc_item = it->begin() + 1; scc_item != it->end(); ++scc_item) {
@@ -128,14 +119,14 @@ pair<int, int> graph::solution() {
     }
     ways *= count_min;
   }
-  return pair<int, int>(cost, ways);
+  return pair<int64_t, int64_t>(cost, ways);
 }
 
 int main() {
-  int junctions, edges;
+  int64_t junctions, edges;
 
   cin >> junctions;
-  vector<int> costs(junctions, 0);
+  vector<int64_t> costs(junctions, 0);
 
   for (auto i = 0; i < junctions; i++)
     cin >> costs[i];
@@ -143,30 +134,32 @@ int main() {
   graph graph(junctions, costs);
 
   cin >> edges;
-  for (int i = 0; i < edges; i++) {
-    int u, v;
+  for (int64_t i = 0; i < edges; i++) {
+    int64_t u, v;
     cin >> u >> v;
     graph.process_edge(u - 1, v - 1);
   }
   
-  for (int i = 0; i < junctions; i++) {
+  for (int64_t i = 0; i < junctions; i++) {
     if (!graph.visited[i])
       graph.dfs(i);
   }
 
-  for (int i = 0; i < junctions; i++) {
-    cout << "[" << i << "] =" << graph.f[i].time << endl;
+  graph.visited.clear();
+  graph.visited.resize(junctions, false);
+  for (const auto& key : graph.f) {
+    graph.dfs_t(key.second);
+    graph.sccs_counter++;
   }
-  for (int i = 0; i < junctions; i++, graph.sccs_counter++) {
-    //    graph.sccs[graph.sccs_counter]; // TODO: how initalize
-    //cout << endl << " { ";
-    graph.dfs_t(i);
-    //cout << " } " << endl;
-  }
-
-  //TODO: Order all scc by costs
-  pair<int, int> solution = graph.solution();
-  cout << solution.first << " " << solution.second << endl;
+  
+  // for (int i = junctions * 2 + 2 - 1; i >= 0; i--) {
+  //   if (graph.f[i] == -1)
+  //     continue;
+  //   graph.dfs_t(graph.f[i]);
+  //   graph.sccs_counter++;
+  // }
+  pair<int64_t, int64_t> solution = graph.solution();
+  cout << solution.first << " " << solution.second % 1000000007 << endl;
   return 0;
 }
 
