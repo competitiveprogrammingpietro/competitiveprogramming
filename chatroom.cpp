b49f51991e7e67cac61f6557a6a6951e721c5b79
Pietro Paolini <competitiveprogramming.pietro@gmail.com>
2018-07-03 11:06:44 +0100

    Chatroom: OK

---
+++ chatroom.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string hello("hello");
+
+int main()
+{
+  string input;
+  int h, i;
+  bool result;
+  
+  h = i = 0;
+  cin >> input;
+  
+  //  cout << input;
+
+  while (true) { // O(N)
+    //    cout << "'" << hello[h] << "' = '" << input[i] << "'" << endl;  
+    
+    if (h == hello.length()) {
+      result = true;
+      break;
+    }
+
+    if (i == input.length()) {
+      result = false;
+      break;
+    }
+
+    if (hello[h] == input[i]) {
+      h++;
+      i++;
+    } else {
+      i++;
+    }
+  }
+
+  if (result)
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+  return 0;
+}
