e0b7a6f5b6cd1a865a989d26985ff6e4244bc9c0
Pietro Paolini <competitiveprogramming.pietro@gmail.com>
2018-07-05 11:07:37 +0100

    Knapsack: OK

--- knapsack.cpp
+++ knapsack.cpp
@@ -21,22 +21,30 @@ int test()
 
   for (int i = 1; i < number + 1; i++) {
     for (int j = 1; j < capacity + 1; j++) {
-      int previous_item = K[i - i][capacity];
-      int add_item = K[i - 1][capacity - weights[i]] + values[i];
-      K[i][j] = previous_item > add_item ? previous_item : add_item;
+      int value_one, value_two;
+      value_one = K[i - 1][j];
+
+      int idx = j - weights[i - 1];
+      if (idx < 0) {
+	K[i][j] = value_one;
+      } else {
+	value_two = K[i - 1][j - weights[i - 1]] + values[i - 1];
+	K[i][j] = value_one > value_two ? value_one : value_two;
+      }
     }
   }
 
-  cout << endl;
+  //  cout << endl;
   int max = 0;
-  for (int i = 1; i < number + 1; i++) {
-    for (int j = 1; j < capacity + 1; j++) {
+  for (int i = 0; i < number + 1; i++) {
+    for (int j = 0; j < capacity + 1; j++) {
       max = K[i][j] > max ? K[i][j] : max;
-      cout << K[i][j] << " ";
+      //      cout << K[i][j] << " ";
     }
-    cout << endl;
+    //    cout << endl;
   }
 
+  cout << max << endl;
   return 0;
 }
 
