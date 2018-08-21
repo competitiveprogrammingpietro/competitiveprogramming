fb6381a9bb3be7e2e1d7db0953b72e79f859bd68
Pietro <pietro>
2018-08-21 18:03:30 +0100

    Vertex cover

--- vertex_cover.cpp
+++ vertex_cover.cpp
@@ -1,53 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 
 /*
+ * TODO: Ask professor
  * https://www.spoj.com/problems/PT07X/
- *
+ * Complexity O(V + E)
  */
 
 using namespace std;
-static int N[100][100] = {{ 0 }};
-int size;
 
-int f(int node) 
-{
-  int edge_present = 0;
-  vector<int> children;
-  
-  for (int i = 0; i < size; i++) {
-    if (N[node][i] != 0)
-      edge_present = 1;
-
-    // Cancel all edges related to me
-    children.push_back(i);
-    N[node][i] = N[i][node] = 0;
-  }
-
-  // There are not edges this node can "serve"
-  if (!edge_present)
-    return 0;
-
-  // All my edges have been canceled, proceed with children
-  int result = 0;
-  for (auto it = children.begin();  it != children.end(); ++it) {
-    result += f(*it);
-  }
-  return result + 1;
-}
 
 int main()
 {
+  int size;
+  vector<int> nodes;
+  int** N;
   
   cin >> size;
+  N = new int*[size + 1]();
+  for (int i = 1; i < size + 1; i++) {
+    N[i] = new int[size + 1]();
+  }
   for (int i = 0; i < size - 1; i++) {
     int u, v;
     cin >> u >> v;
-    u--;v--;
     N[u][v] = N[v][u] = 1;
+    //    cout << "(" << u << "," << v << ")" << N[u][v] << endl;
   }
 
-  int res = f(0);
-  cout << res << endl;
+  //  cout << "-----" << endl;
+  for (int i = 1; i < size + 1; i++) {
+
+    // Pick the first edge up and remove all the incident edges
+    for (int j = 1; j < size + 1; j++) {
+      //      cout << "(" << i << "," << j << ")" << N[i][j] << endl;
+
+      if (N[i][j] == 0)
+	continue;
+      
+      nodes.push_back(i);
+      nodes.push_back(j);
+
+      for (int z = 1; z < size + 1; z++) {
+
+	// Columns
+	N[z][i] = N[z][j] = 0;
+
+	//Rows
+	N[i][z] = N[j][z] = 0;
+      }
+    }
+  }
+  //  cout << "-----" << endl;
+  cout << nodes.size() << endl;
   return 0;
 }
