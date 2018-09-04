53188ae7851f76e0955d49af1a51d3756652d5e1
Pietro Paolini <pedr0@pedr0OpenBSD.my.domain>
2018-09-04 23:16:04 +0100

    Vertex cover

--- vertex_cover.cpp
+++ vertex_cover.cpp
@@ -3,9 +3,10 @@
 #include <utility>
 
 /*
- * TODO: Ask professor
  * https://www.spoj.com/problems/PT07X/
  * Complexity O(V + E)
+ * TODO: not quite clear why it does not pass last case
+ * http://discuss.spoj.com/t/vertex-cover/30039 
  */
 
 using namespace std;
@@ -16,43 +17,73 @@ int main()
   int size;
   vector<int> nodes;
   int** N;
+  int *V;
   
   cin >> size;
+
+  if (size == 1) {
+    cout << 1 << endl;
+    return 0;
+  }
   N = new int*[size + 1]();
+  V = new int[size + 1]();
+  
   for (int i = 1; i < size + 1; i++) {
     N[i] = new int[size + 1]();
+    V[i] = false;
   }
   for (int i = 0; i < size - 1; i++) {
     int u, v;
     cin >> u >> v;
-    N[u][v] = N[v][u] = 1;
-    //    cout << "(" << u << "," << v << ")" << N[u][v] << endl;
+    N[u][v] = 1;
+    N[v][u] = 1;
+    //    cout << "(" << u << "," << v << ")" << N[v][u] << endl;// Sth wrong here 
   }
 
+  // for (int i = 1; i < size + 1; i++) {
+  //   for (int j = 0; j < size + 1; j++) {
+  //     cout << N[i][j] << " ";
+  //   }
+  //   cout << endl;
+  // }
   //  cout << "-----" << endl;
   for (int i = 1; i < size + 1; i++) {
 
+    if (V[i])
+      continue;
+    
     // Pick the first edge up and remove all the incident edges
     for (int j = 1; j < size + 1; j++) {
-      //      cout << "(" << i << "," << j << ")" << N[i][j] << endl;
 
       if (N[i][j] == 0)
 	continue;
-      
+
+      if (V[j]) 
+	continue;
+
+      //      cout << "Examining " << i << "," << j << endl;
       nodes.push_back(i);
       nodes.push_back(j);
-
+      V[i] = V[j] = 1;
+     
       for (int z = 1; z < size + 1; z++) {
 
-	// Columns
-	N[z][i] = N[z][j] = 0;
+      	// Columns
+      	N[z][i] = N[z][j] = 0;
 
-	//Rows
-	N[i][z] = N[j][z] = 0;
+      	//Rows
+      	N[i][z] = N[j][z] = 0;
       }
     }
+    // cout << "-------------------" << endl;
+    // for (int i = 1; i < size + 1; i++) {
+    //   for (int j = 0; j < size + 1; j++) {
+    // 	cout << N[i][j] << " ";
+    //   }
+    //   cout << endl;
+    // }
+    // cout << "-------------------" << endl;
   }
-  //  cout << "-----" << endl;
   cout << nodes.size() << endl;
   return 0;
 }
